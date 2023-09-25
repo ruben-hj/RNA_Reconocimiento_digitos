cb6d4765d33ebdd400a90a231a9622179d9924a8
ruben-hj <ruben.herreji@gmail.com>
2023-09-25 01:41:41 -0600

    Red densa secuencial con Keras

---
+++ RedDensa.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.optimizers import  Adam
+import numpy as np
+
+dataset=mnist.load_data() #Cargamos el conjunto de datos MNIST y lo almacenamos en la variable "dataset"
+
+dat=np.array(dataset)
+(x_train, y_train), (x_test, y_test) = dataset # Descomponemos el conjunto de datos en datos de entrenamiento y prueba
+
+#Definimos hiperparámetros para el entrenamiento
+learning_rate = 0.001
+epochs = 10
+batch_size = 120
+
+# Definimos hiperparámetros para el entrenamiento
+x_trainv = x_train.reshape(60000, 784)
+x_testv = x_test.reshape(10000, 784)
+# Convertimos los datos de entrada a tipo float32
+x_trainv = x_trainv.astype('float32')
+x_testv = x_testv.astype('float32')
+
+#Normalizamos los datos
+x_trainv /= 255  # x_trainv = x_trainv/255
+x_testv /= 255
+
+# Convertimos las etiquetas de clase a codificación one-hot
+num_classes=10
+y_trainc = keras.utils.to_categorical(y_train, num_classes)
+y_testc = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(128, activation='sigmoid', input_shape=(784,))) #Capa densa con 128 neuronas
+model.add(Dense(num_classes, activation='softmax'))
+#model.summary() #Desglosa la estructura de nuestro modelo
+
+# Compilamos el modelo especificando la función de pérdida, optimizador y métrica de evaluación
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(learning_rate=learning_rate),
+              metrics=['accuracy'])
+
+# Entrenamos el modelo en los datos de entrenamiento
+history = model.fit(x_trainv, y_trainc,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_testv, y_testc)
+                   )
+
+test_loss, test_accuracy = model.evaluate(x_testv, y_testc)
+print(f"Pérdida en el conjunto de prueba: {test_loss}")
+print(f"Precisión en el conjunto de prueba: {test_accuracy}")
+
+#Evaluamos la eficiencia del modelo
+'''score = model.evaluate(x_testv, y_testc, verbose=1) #evaluar la eficiencia del modelo
+print(score)
+a=model.predict(x_testv) #predicción de la red entrenada
+print(a.shape)
+print(a[1])
+print("resultado correcto:")
+print(y_testc[1])'''
